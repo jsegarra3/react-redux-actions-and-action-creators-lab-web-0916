4da43935a7774211326aa560f181ccca7be3add0
James Segarra III <JamesSegarra@gmail.com>
2016-11-21 14:33:06 -0500

    Done.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,25 @@
 // define your actions here!
+export function addMovie(movie) {
+  return {
+    type: 'ADD_MOVIE',
+    movie: {title: movie.title}
+  }
+}
+export function deleteMovie(movie) {
+  return {
+    type: 'DELETE_MOVIE',
+    movie: {title: movie.title}
+  }
+}
+export function updateUsername(username) {
+  return {
+    type: 'UPDATE_USERNAME',
+    username: username
+  }
+}
+export function resetUsername() {
+  return {
+    type: 'RESET_USERNAME',
+    username: 'default'
+  }
+}
